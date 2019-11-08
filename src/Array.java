e1278e944df3e759bec000495d2787b5abf1e294
thefluffyoshi <katielynntrinh@gmail.com>
2019-11-08 13:53:10 -0800

    in progress

--- src/Array.java
+++ src/Array.java
@@ -1,6 +1,12 @@
 public class Array
 {
+    final static int LIMIT = 50;
     public static void main (String[] args)
     {
+        int[] list = new int[LIMIT];
+        for (int index = 0; index < LIMIT; index++)
+        {
+
+        }//end for loop
     }//end main
 }//end class Array
