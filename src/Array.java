c86673261ba958bc3ac2fc8851de7a9853531298
thefluffyoshi <katielynntrinh@gmail.com>
2019-11-19 13:22:42 -0800

    Done

--- src/Array.java
+++ src/Array.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
 public class Array
 {
-    final static int LIMIT = 50;
     public static void main (String[] args)
     {
-        int[] list = new int[LIMIT];
+        //Scanner
+        Scanner keyboard = new Scanner(System.in);
+        //array
+        int nums[] = new int[5];
+
+        for (int i = 0; i < nums.length; i++)
+        {
+            System.out.println("Please enter a number for the array ");
+            nums[i] = keyboard.nextInt();
+        }//end for loop 1
+
+        System.out.println("The numbers for the array are: ");
+        for (int i = 0; i < nums.length; i++)
+        {
+            System.out.println(+ nums[i] + ",");
+        }//end for loop 2
     }//end main
 }//end class Array
